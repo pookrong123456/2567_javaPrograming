54f67cf6c8a163147d3e89b56c30a649d17a80cd
pookrong123456 <sfx16836@gmail.com>
2025-02-19 08:33:01 +0700

    Attendence

---
+++ LabSheet9/src/Director.java
@@ -0,0 +1,46 @@
+public class Director {
+    private String name;
+    private String email;
+    private char gender;
+
+    
+    public Director(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    
+    public String getGenderName() {
+        if (gender == 'm' || gender == 'M') {
+            return "Male";
+        } else if (gender == 'f' || gender == 'F') {
+            return "Female";
+        } else {
+            return null;
+            
+        }
+        
+    }
+    public String toString() {
+        return String.format("%s (%s;%s)", getName(), getEmail(), getGenderName());
+    }
+    
+
+    
+   
+
+    
+    
+
+ 
+    
+}
+
+
