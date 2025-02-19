3cd7b1ef9aa7aa0ce9ad6bd756677bebcfc6c52d
pookrong123456 <sfx16836@gmail.com>
2025-02-19 10:13:24 +0700

    Leb9

--- LabSheet9/src/Director.java
+++ LabSheet9/src/Director.java
@@ -1,46 +1,38 @@
 public class Director {
-    private String name;
-    private String email;
-    private char gender;
+	 private String email;
+	    private char gender;
+	    private String name;
+	 
+	    Director(String name, String email,char gender) {
+	        this.name = name;
+	        this.email = email;
+	        this.gender = gender;
+	    }
+	    Director(String name, String email) {
+	        this(name,email,' ');
+	    }
+	    public String getName() {
+	        return this.name;
+	    }
+	    public String getEmail() {
+	        return this.email;
+	    }
+	    public String getGenderName() {
+	    if (this.gender == 'm' || this.gender == 'M' ) {
+	            return "Male" ;
+	        }
+	        else if (this.gender == 'f' || this.gender == 'F') {
+	            return "Female" ;
+	        }
+	        else {
+	            return null;
+	        }
+	    }
+	    public String toString() {
+	        return getName() + " ("+getEmail()+";"+getGenderName()+")";
+	    }
 
-    
-    public Director(String name, String email, char gender) {
-        this.name = name;
-        this.email = email;
-        this.gender = gender;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getEmail() {
-        return email;
-    }
 
-    
-    public String getGenderName() {
-        if (gender == 'm' || gender == 'M') {
-            return "Male";
-        } else if (gender == 'f' || gender == 'F') {
-            return "Female";
-        } else {
-            return null;
-            
-        }
-        
-    }
-    public String toString() {
-        return String.format("%s (%s;%s)", getName(), getEmail(), getGenderName());
-    }
-    
-
-    
-   
-
-    
-    
-
- 
-    
-}
+	}
 
 
--- LabSheet9/src/MovieDemo.java
+++ LabSheet9/src/MovieDemo.java
@@ -1,4 +1,39 @@
-
+import java.util.*;
 public class MovieDemo {
-
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input movie id	: ");
+		String movieId = input.next();
+		System.out.print("Input movie name : ");
+		String movieName = input.next();
+		System.out.println();
+		input.nextLine();
+		System.out.print("Input director name	:");
+		String directorName = input.next();
+		input.nextLine();
+		System.out.print("Input director e-mail	:");
+		String directorEmail = input.next();
+		input.nextLine();
+		System.out.print("Input director gender : ");
+		char gender = input.next().toUpperCase().charAt(0);
+		while (true) {
+			if(gender == 'M'|| gender=='F') {break;}
+			System.out.print("Input director gender, again : ");
+			 gender = input.next().toUpperCase().charAt(0);
+		}
+		System.out.println();
+		System.out.print("Input movie theater no. : ");
+		int movieTheaterNo = input.nextInt();
+        while (true) {
+        	if (movieTheaterNo >= 1 && movieTheaterNo <= 15) 
+        	{break;}
+            System.out.print("Please input 1 - 15 only. : ");
+            movieTheaterNo = input.nextInt();
+        }
+		System.out.println();
+        Director director = new Director(directorName, directorEmail, gender);
+        Theater theater = new Theater(movieId, movieName, director, movieTheaterNo);
+        System.out.print(theater.toString());
+        input.close();
+	}
 }
